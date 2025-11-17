3ada8134cc0c34945de264d8e5236c4610e2fdb0
Dasukiii <dasuki40603@gmail.com>
2025-11-17 17:39:32 +0800

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -352,7 +352,7 @@ function AuthModalIntegrated({
             <img 
               src="kadosh-ai-icon.png" // This path assumes your icon is in the 'public' folder
               alt="Kadosh AI" 
-              className="w-20 h-20" // You can adjust the size here
+              className="w-5 h-5" // You can adjust the size here
             />
           </a>
         </div>
